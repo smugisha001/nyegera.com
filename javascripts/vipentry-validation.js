ed4f7d88814a88061feb8b50e7b4c230a5fb0f0f
smugisha001 <148874622+smugisha001@users.noreply.github.com>
2023-10-24 15:01:37 +0200

    Add files via upload

---
+++ javascripts/vipentry-validation.js
@@ -0,0 +1,41 @@
+
+function vipentry(){
+    var n = document.getElementById("namesid").value;
+    var c = document.getElementById("codethree").value;
+
+    if ( n === "SAMUEL MUGISHA" && c === "130") { alert("Washyizwe muri system. Code ya nimero yawe izarangira mu minsi 3 uhereye nyuma yumunsi wishyuriyeho. Kuwa 4. Gukomeza, kanda Ok. Murakoze")
+        window.location = "./vipcodethree1/vip ms-130 thur21.10.2023.html"; return false;} 
+    
+        if ( n === "SAMUELMUGISHA" && c === "130") {  alert("Washyizwe muri system. Code ya nimero yawe izarangira mu minsi 3 uhereye nyuma yumunsi wishyuriyeho. Kuwa 4. Gukomeza, kanda Ok. Murakoze")
+        window.location = "./vipcodethree1/vip ms-130 thur21.10.2023.html"; return false;}  //to be expired tuesday 24.10.2023
+        
+            else {
+            alert("Byashoboka ko winjijemo ibikuranga nabi cyangwa ukaba utarashyirwa muri system. Murakoze kubwo gutegereza, mu gihe turi kubikoraho.");
+         } 
+        
+        
+    
+}
+
+function vipcaution(){
+    alert("VIP room irishyurwa. Igiceri (1) 100RWF rimara iminsi 3 uyisura. Nyuma yo kwishyura utegereze amasaha 24 kugira ushyirwe muri system. VIP agirwa inama vuba adategereje abamubanjirije kubaza.");
+    return false;
+}
+
+function linkcontacterror(){
+    alert("Ntabwo urubuga ruratunganywa neza. Byaba byiza ubaye udusuye kuri facebook. Ibuto igerayo ni ibanza. Murakoze");
+    return false;
+}
+
+function contactoption(){
+    alert("Muriki cyumba urabona uburyo watugeraho kuzindi mbuga nuko waduha inyunganizi, n'ibitekerezo. Urabonamo kandi uburyo wareba video igusobanurira uko urubuga rukora. Murakoze");
+    return false;
+}
+
+function supportoption(){
+    alert("Muriki cyumba urabona uburyo watugezaho inkunga ndetse nuko washyigikira uru rubuga rukarushaho gutera imbere no kuduha service nziza. Inkunga iyo ari yo yose nimbaraga kuri twe. Dushyigikire uyu munsi. Murakoze");
+    return false;
+}
+
+
+
